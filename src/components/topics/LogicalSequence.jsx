039a27eb5e3f355e1ff3a5ec70c99e4e5827eb73
pranee17 <bpraneetha17@gmail.com>
2024-11-27 13:23:51 +0530

    Updated Logical Sequence page

--- src/components/topics/LogicalSequence.jsx
+++ src/components/topics/LogicalSequence.jsx
@@ -176,38 +176,7 @@ export const questions = [
         { name: "Jane Smith", comment: "Great explanation, I agree!" }
       ]
     },
-    {
-      question: (
-        <>
-          Arrange the words given below in a meaningful sequence.<br /> <br />
-          1. <span>Word</span> 2. <span>Paragraph</span> 3. <span>Sentence</span> <br />
-          4. <span>Letters</span> 5. <span>Phrase</span>
-        </>
-      ),
-      options: ["4,1,5,2,3", "4,1,3,5,2", "4,2,5,1,3", "4,1,5,3,2"],
-      correctAnswer: 2,
-      explanation: "The correct order  is: Letter ,Word , Phrase, Sentence  , Paragraph .",
-      discussion: [
-        { name: "John Doe", comment: "This is how the words logically follow each other in a sequence." },
-        { name: "Jane Smith", comment: "Great explanation, I agree!" }
-      ]
-    },
-    {
-      question: (
-        <>
-          Arrange the words given below in a meaningful sequence.<br /> <br />
-          1. <span>Word</span> 2. <span>Paragraph</span> 3. <span>Sentence</span> <br />
-          4. <span>Letters</span> 5. <span>Phrase</span>
-        </>
-      ),
-      options: ["4,1,5,2,3", "4,1,3,5,2", "4,2,5,1,3", "4,1,5,3,2"],
-      correctAnswer: 2,
-      explanation: "The correct order  is: Letter ,Word , Phrase, Sentence  , Paragraph .",
-      discussion: [
-        { name: "John Doe", comment: "This is how the words logically follow each other in a sequence." },
-        { name: "Jane Smith", comment: "Great explanation, I agree!" }
-      ]
-    },
+  
   
     // Add more questions as needed
   ];
